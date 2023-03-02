4c9f2b56bacbcf1ef4b0113b3f45509116ee164e
Goyhman Peter <pgoyhman@mail.ru>
2023-03-02 13:38:46 +0300

    readme

---
+++ program.java
@@ -0,0 +1,37 @@
+/**
+ * program
+ */
+import java.util.Scanner;
+public class program {
+    public static void main(String[] args) {
+        //String s = null;    строка
+        //System.out.println(s);   
+        //float f = 12.45f;    вещественные числа
+        //double f = 12.45;    вещественные числа
+        //System.out.println(f);
+        //char ch = '{';       типизация какого-то символа
+        // char ch = 123;
+        //System.out.println(ch);
+        // String msg = "Hello world";
+        // System.out.println(msg);
+        //  var i = 123;    неявная типизация
+        //  System.out.println(i);
+        //  int[] arr = new int[10] {1,2,3};   массивы
+        //  System.out.println(arr[3]);
+        // int[] arr[] = new int[3] [5]; двумерные массивы
+
+    
+              
+        //  Scanner iScanner = new Scanner(System.in);      Получение данных из терминала
+        //  System.out.printf("name: ");
+        //  String name = iScanner.nextLine();
+        //  System.out.printf("Привет, %s!", name);
+        //  iScanner.close();
+    //  String s = "qwe";
+    //  int a = 123;                Форматированный выход
+    //  String q = s + a;
+    //  System.out.println(q);
+
+    }
+    
+}
